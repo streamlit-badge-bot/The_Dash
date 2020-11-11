7ee9ed1d8de2d1d81e67a6a489714d61f2a617c5
michael-william <56475992+michael-william@users.noreply.github.com>
2020-11-11 16:54:54 +0100

    update

--- the_dash.py
+++ the_dash.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import time
 from datetime import datetime
 import datetime
 from datetime import timedelta
@@ -10,7 +11,7 @@ from string import ascii_lowercase
 import types
 import random
 import dataclasses
-
+import SessionState
 
 # Plotting
 import plotly.graph_objs as go
@@ -19,35 +20,54 @@ import plotly.io as pio
 import plotly.offline as pyo
 pio.renderers.default = 'iframe'
 
+@st.cache(allow_output_mutation=True)
+def data():
+	team_df = pd.read_excel('https://github.com/michael-william/The_Dash/blob/master/resources/teams.xlsx?raw=true')
+	dashes = pd.read_excel('https://github.com/michael-william/The_Dash/blob/master/resources/dashes.xlsx?raw=true')
+	dashes_list = dashes.Dash.to_list()
+	return team_df, dashes_list
 
-st.sidebar.title('Navigation')
-selection = st.sidebar.radio("Go to", ['Setup', 'Game'])
+team_df, dashes_list = data()
 
+teams = team_df[['Team','Name']].sort_values('Team').reset_index(drop=True)
+gummies = team_df[team_df.Team=='Gummies']['Name'].reset_index(drop=True)
+threemoney = team_df[team_df.Team=='Three Money']['Name'].reset_index(drop=True)
+
+def turn_order():
+	turn_order = []
+	unique_names = 4
+	for i in range(unique_names):
+		turn_order.append(gummies.iloc[i])
+		turn_order.append(threemoney.iloc[i])
+	return turn_order
+
+turn_order = turn_order()
+x=0
+ss = SessionState.get(x=x)
+
+def shuffle(x):
+    new = x
+    random.shuffle(new)
+    return new
+
+r1 = shuffle(dashes_list)
+r2 = shuffle(dashes_list)
+r3 = shuffle(dashes_list)
 
 def main():
-	team_df = pd.DataFrame(columns=['Name','Team'])
-	def setup_features():
-		usr_name = st.text_input("Your name", "Mancy Buckles")
-		usr_team = st.selectbox('Team',['Gummies', 'Three Money'])
-		return usr_name, usr_team
 
-	
-	def team_setup():
-		temp_df = team_df
-		if usr_name in team_df.Name:
-			temp_df.loc[temp_df.Name==usr_name,'Team'] = usr_team
+	if st.button('Next Word!'):
+		ss.x = ss.x + 1
+		if ss.x>20:
+			ss.x = 0
+		st.write(r1[ss.x])
+	if st.button('Last Word'):
+		if ss.x>0:
+			ss.x = ss.x-1
 		else:
-			temp_df = temp_df.append({'Name': usr_name,'Team':usr_team}, ignore_index=True)
-		return temp_df
+			ss.x = 0
+		st.write(r1[ss.x])
 
-
-	if selection == 'Setup':
-		usr_name, usr_team = setup_features()
-		if st.button('Update yourself!'):
-			temp_df = team_setup()
-			team_df = pd.concat(team_df,temp_df)
-
-		st.write(team_df)
 
 if __name__ == "__main__":
     main()
