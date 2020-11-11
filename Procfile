04a5e31617430a604dc8edf10fd471f74fe77f87
michael-william <56475992+michael-william@users.noreply.github.com>
2020-11-11 19:42:22 +0100

    updated

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run the_dash.py
--- the_dash.py
+++ the_dash.py
@@ -1,24 +1,13 @@
 import pandas as pd
 import numpy as np
-import time
-from datetime import datetime
-import datetime
-from datetime import timedelta
 import streamlit as st
-from resources.gamestate import persistent_game_state
-from typing import List, Tuple
-from string import ascii_lowercase
-import types
 import random
-import dataclasses
 import SessionState
 
-# Plotting
-import plotly.graph_objs as go
-from plotly.subplots import make_subplots
-import plotly.io as pio
-import plotly.offline as pyo
-pio.renderers.default = 'iframe'
+st.markdown('[![datandstories](https://github.com/michael-william/CBD/raw/master/resources/DS_logo_200.png)](https://dataandstories.com)', unsafe_allow_html=True)
+st.header("""
+**The Dash**
+""")
 
 @st.cache(allow_output_mutation=True)
 def data():
@@ -42,31 +31,86 @@ def turn_order():
 	return turn_order
 
 turn_order = turn_order()
-x=0
+x=-1
 ss = SessionState.get(x=x)
 
-def shuffle(x):
-    new = x
-    random.shuffle(new)
-    return new
+@st.cache()
+def shuffle(x,num):
+    r1 = random.sample(x, num)
+    r2 = random.sample(x, num)
+    r3 = random.sample(x, num)
+    return r1, r2, r3
 
-r1 = shuffle(dashes_list)
-r2 = shuffle(dashes_list)
-r3 = shuffle(dashes_list)
+r1, r2, r3 = shuffle(dashes_list,20)
+
+
 
 def main():
 
-	if st.button('Next Word!'):
-		ss.x = ss.x + 1
-		if ss.x>20:
-			ss.x = 0
-		st.write(r1[ss.x])
-	if st.button('Last Word'):
-		if ss.x>0:
-			ss.x = ss.x-1
+	round_type = st.selectbox('Round',('Test','Description','Charades','1 Word'))
+	current_round = round_type
+	if current_round =='Test':
+		r=['This is a sample dash', 'cruise missile lana', 'gypsy Molly', 'Mike is the greatest!']
+		st.header("This is a test round")
+		st.write("1) If you are the first person in the round, click the plus sign for the next word")
+		st.write("2) If you are not the first person, enter the clue count to start")
+		st.write("3) After 60 seconds, your turn is over")
+		st.write("4) Relay the clue count to the group and place your phone face down on the table")
+		ss.x= st.number_input('Clue Count',min_value=-1, max_value=4, value=-1,step=1)
+		if ss.x==-1:
+			st.markdown('**Start the round!**')
+		elif ss.x ==4:
+			st.markdown('**End of Round**!')
+			st.balloons()
 		else:
-			ss.x = 0
-		st.write(r1[ss.x])
+			st.markdown('**{}**'.format(r[ss.x]))
+	elif current_round =='Description':
+		r=r1
+		st.header("Welcome to the 1st Round of The Dash")
+		st.write("1) If you are the first person in the round, click the plus sign for the next word")
+		st.write("2) If you are not the first person, enter the clue count to start")
+		st.write("3) After 60 seconds, your turn is over")
+		st.write("4) Relay the clue count to the group and place your phone face down on the table")
+		ss.x= st.number_input('Clue Count',min_value=-1, max_value=20, value=-1,step=1)
+		if ss.x==-1:
+			st.markdown('**Start the round!**')
+		elif ss.x ==20:
+			st.markdown('**End of Round**!')
+			st.balloons()
+		else:
+			st.write(r[ss.x])
+	elif current_round =='Charades':
+		r=r2
+		st.header("Welcome to the 2nd Round of The Dash")
+		st.write("Use normal charades to help your team guess the answer")
+		st.write("1) If you are the first person in the round, click the plus sign for the next word")
+		st.write("2) If you are not the first person, enter the clue count to start")
+		st.write("3) After 60 seconds, your turn is over")
+		st.write("4) Relay the clue count to the group and place your phone face down on the table")
+		ss.x= st.number_input('Clue Count',min_value=-1, max_value=20, value=-1,step=1)
+		if ss.x==-1:
+			st.markdown('**Start the round!**')
+		elif ss.x ==20:
+			st.markdown('**End of Round**!')
+			st.balloons()
+		else:
+			st.write(r[ss.x])
+	else:
+		r=r3
+		st.header("Welcome to the Last Round of The Dash!")
+		st.write("Use only one word,including proper nouns, to help your team guess the clue")
+		st.write("1) If you are the first person in the round, click the plus sign for the next word")
+		st.write("2) If you are not the first person, enter the clue count to start")
+		st.write("3) After 60 seconds, your turn is over")
+		st.write("4) Relay the clue count to the group and place your phone face down on the table")
+		ss.x= st.number_input('Clue Count',min_value=-1, max_value=20, value=-1,step=1)
+		if ss.x==-1:
+			st.markdown('**Start the round!**')
+		elif ss.x ==20:
+			st.markdown('**End of Round**!')
+			st.balloons()
+		else:
+			st.write(r[ss.x])
 
 
 if __name__ == "__main__":
